2aaf05512de605cd5c2850a3f847b4ef1accc5c7
Geronimo <lopez.geronimo213@gmail.com>
2023-09-19 18:10:46 -0500

    Changed bootstrap theme. Created db. Created categories table. Implemented Create and Receive of categories table entries.

---
+++ MVC Online Bookshop/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Online_Bookshop.Data;
+using MVC_Online_Bookshop.Models;
+
+namespace MVC_Online_Bookshop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDBContext _dbContext;
+        public CategoryController(AppDBContext dB)
+        {
+            _dbContext = dB;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _dbContext.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(obj);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+    }
+}
---
+++ MVC Online Bookshop/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Online_Bookshop.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "A name for the category is required.")]
+        [DisplayName("Category Name")]
+        [MaxLength(30, ErrorMessage = "Category name must be 30 characters or less.")]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Order must be between 1 and 100.")]
+        public int DisplayOrder { get; set; }
+    }
+}
