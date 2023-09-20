b7cca5a347ce5022499a87eaa14b999ef6cf5fc0
Geronimo <lopez.geronimo213@gmail.com>
2023-09-20 12:27:14 -0500

    Added comments to CategoryController. Also changed implementation of Delete Category POST method. Now accepts entire POSTed model as opposed to only the id.

--- MVC Online Bookshop/Controllers/CategoryController.cs
+++ MVC Online Bookshop/Controllers/CategoryController.cs
@@ -11,17 +11,27 @@ namespace MVC_Online_Bookshop.Controllers
         {
             _dbContext = dB;
         }
+
+        /************************************
+        RECIEVE CATEGORIES (INDEX)
+        ************************************/
+
+        //Handler for main Category page
         public IActionResult Index()
         {
             List<Category> objCategoryList = _dbContext.Categories.ToList();
             return View(objCategoryList);
         }
 
+        /************************************
+        CREATE CATEGORY
+        ************************************/
+        //Get result for Create Category page
         public IActionResult Create()
         {
             return View();
         }
-
+        //Post method handler for Create Category, being passed a Category to work with
         [HttpPost]
         public IActionResult Create(Category obj)
         {
@@ -29,6 +39,7 @@ namespace MVC_Online_Bookshop.Controllers
             {
                 _dbContext.Categories.Add(obj);
                 _dbContext.SaveChanges();
+                //Save to tempdata for accessing on next view w/ toastr.
                 TempData["success"] = $"Category {obj.Name} created successfully!";
                 return RedirectToAction("Index", "Category");
             }
@@ -36,43 +47,54 @@ namespace MVC_Online_Bookshop.Controllers
         }
 
 
+
+        /************************************
+        UPDATE CATEGORY
+        ************************************/
+        //Get for Edit Category page. Takes in an id passed to it from form via POST.
         public IActionResult Edit(int? id)
         {
             if(id == null || id == 0)
             {
                 return NotFound();
             }
-            Category categoryFromDb = _dbContext.Categories.Find(id);
-            if (categoryFromDb == null) { return NotFound(); }
+            Category? categoryFromDb = _dbContext.Categories.Find(id); //Nullable category, which is checked on next line.
+            if (categoryFromDb == null) { return NotFound(); } //If find returned null, NotFound
             return View(categoryFromDb);
         }
 
         [HttpPost]
         public IActionResult Edit(Category obj)
         {
-            _dbContext.Categories.Update(obj);
+            _dbContext.Categories.Update(obj); //Update the model and save changes.
             _dbContext.SaveChanges();
             TempData["success"] = $"Category {obj.Name} updated successfully!";
             return RedirectToAction("Index", "Category");
         }
 
+
+
+        /************************************
+        DELETE CATEGORY
+        ************************************/
         public IActionResult Delete(int? id)
         {
-            if (id == null || id == 0)
+            if (id == null || id == 0) //Null check for id
             {
                 return NotFound();
             }
-            Category categoryFromDb = _dbContext.Categories.Find(id);
+            Category? categoryFromDb = _dbContext.Categories.Find(id); //If find returns null, NotFound
             if (categoryFromDb == null) { return NotFound(); }
 
             return View(categoryFromDb);
         }
 
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeletePOST(int? id)
+        [HttpPost]
+        public IActionResult Delete(Category toDelete)
         {
-            Category? toDelete = _dbContext.Categories.Find(id);
-            if (toDelete == null) { return NotFound(); }
+            //Category? toDelete = _dbContext.Categories.Find(id); //Old code from messing with retrieving specific fields from POSTed model as parameter.
+            //if (toDelete == null) { return NotFound(); }
+
             _dbContext.Remove(toDelete);
             _dbContext.SaveChanges();
             TempData["success"] = $"Category {toDelete.Name} removed successfully!";
