55aba61e460601bb24c5c5081d331e12ac152352
Geronimo <lopez.geronimo213@gmail.com>
2023-10-05 11:03:14 -0500

    Added entry for Session Cart key.

--- Bookshop.Utility/SD.cs
+++ Bookshop.Utility/SD.cs
@@ -12,5 +12,7 @@ namespace Bookshop.Utility
         public const string Role_Admin = "Admin";
         public const string Role_Employee = "Employee";
 
+        public const string SessionCart = "SessionShoppingCart";
+
     }
 }
