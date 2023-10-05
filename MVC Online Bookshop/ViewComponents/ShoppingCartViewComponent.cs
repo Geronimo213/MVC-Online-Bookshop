845a03caf930f37d3ab00b2e8dc886e9aa40df1c
Geronimo <lopez.geronimo213@gmail.com>
2023-10-05 11:41:22 -0500

    Added and implemented ShoppingCart ViewComponent for use in layout of page. Specifically, nav bar shopping cart + badge.

---
+++ MVC Online Bookshop/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using Bookshop.DataAccess.Repository;
+using Bookshop.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Bookshop.DataAccess.Repository.IRepository;
+
+namespace MVC_Online_Bookshop.ViewComponents
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+        private  IUnitOfWork UnitOfWork { get; }
+
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            this.UnitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim is not null)
+            {
+                if (HttpContext.Session.GetInt32(SD.SessionCart) is null)
+                {
+                    HttpContext.Session.SetInt32(SD.SessionCart,
+                        UnitOfWork.ShoppingCartRepository.GetAll().Count(x => x.UserId == claim.Value));
+                }
+                
+                return View((int)HttpContext.Session.GetInt32(SD.SessionCart)!);
+            }
+
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+        }
+    }
+}
