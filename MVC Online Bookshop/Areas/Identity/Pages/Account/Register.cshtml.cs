ade9af891c660c2881a311eae9e277ba9f65d0a3
Geronimo <lopez.geronimo213@gmail.com>
2023-10-06 14:19:07 -0500

    Revert "Refactor of Role constants."
    This reverts commit 4ac48ccd28d8804804d2bbfc81a1021d35f1534b.

--- MVC Online Bookshop/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MVC Online Bookshop/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -123,11 +123,11 @@ namespace MVC_Online_Bookshop.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
-            if (!_roleManager.RoleExistsAsync(SD.RoleAdmin).GetAwaiter().GetResult())
+            if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
             {
-                _roleManager.CreateAsync(new IdentityRole(SD.RoleAdmin)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.RoleCustomer)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.RoleEmployee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
             }
 
             Input = new()
@@ -168,7 +168,7 @@ namespace MVC_Online_Bookshop.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _userManager.AddToRoleAsync(user,SD.RoleCustomer);
+                        await _userManager.AddToRoleAsync(user,SD.Role_Customer);
                     }
 
                     var userId = await _userManager.GetUserIdAsync(user);
