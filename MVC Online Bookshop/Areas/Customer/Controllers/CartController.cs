50bf6e1d492985e04e6f6e97593e17d64d4413b8
Geronimo <lopez.geronimo213@gmail.com>
2023-10-06 14:16:36 -0500

    Implemented shopping cart item count update and removal.

--- MVC Online Bookshop/Areas/Customer/Controllers/CartController.cs
+++ MVC Online Bookshop/Areas/Customer/Controllers/CartController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using Bookshop.DataAccess.Data;
 using Bookshop.DataAccess.Repository.IRepository;
+using Bookshop.Utility;
 
 namespace MVC_Online_Bookshop.Areas.Customer.Controllers
 {
@@ -21,9 +22,55 @@ namespace MVC_Online_Bookshop.Areas.Customer.Controllers
 
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            List<ShoppingCart> items = UnitOfWork.ShoppingCartRepository.GetAll(includeOperators: "User,Product").Where(x => x.UserId == userId).ToList();
+            List<ShoppingCart> items = UnitOfWork.ShoppingCartRepository.GetAll(includeOperators: "User,Product,Product.Category").Where(x => x.UserId == userId).ToList();
 
             return View(items);
+        }
+
+        [HttpPost]
+        public IActionResult ChangeItemCount(int cartId, string changeAction)
+        {
+            var cartItem =
+                UnitOfWork.ShoppingCartRepository.Get(x => x.Id == cartId);
+            if (cartItem != null)
+            {
+                switch (changeAction)
+                {
+                    case SD.CartIncrement:
+                        cartItem.Count++;
+                        break;
+                    case SD.CartDecrement:
+                        cartItem.Count--;
+                        break;
+                    default:
+                        break;
+                }
+                UnitOfWork.ShoppingCartRepository.Update(cartItem);
+                UnitOfWork.Save();
+                if (cartItem.Count < 1)
+                {
+                    return RemoveItem(cartId);
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult RemoveItem(int cartId)
+        {
+            var cartItem =
+                UnitOfWork.ShoppingCartRepository.Get(x => x.Id == cartId, tracked:true);
+            if (cartItem != null)
+            {
+                UnitOfWork.ShoppingCartRepository.Delete(cartItem);
+                UnitOfWork.Save();
+            }
+            if (HttpContext.Session.GetInt32(SD.SessionCart) is not null)
+            {
+                HttpContext.Session.SetInt32(SD.SessionCart, (HttpContext.Session.GetInt32(SD.SessionCart) ?? 1) - 1);
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
