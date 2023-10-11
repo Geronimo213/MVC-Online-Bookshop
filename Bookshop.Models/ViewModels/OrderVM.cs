e1f4f7d2575d9f156a66c29b7f549bab192c6e8f
Geronimo <lopez.geronimo213@gmail.com>
2023-10-11 17:57:12 -0500

    model used to store complete report of header and lines.

---
+++ Bookshop.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookshop.Models.ViewModels
+{
+    public class OrderVM
+    {
+        public Order Header { get; set; }
+        public IEnumerable<OrderLines> Lines { get; set; }
+
+        public OrderVM()
+        {
+            this.Header = new Order();
+            this.Lines = new List<OrderLines>();
+        }
+    }
+}
