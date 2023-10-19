ddc9f1fc5be50cb80047f591cc618a1be4929e0a
Geronimo <lopez.geronimo213@gmail.com>
2023-10-19 10:12:56 -0500

    Added overload for product repository Add, Update accepting single argument ProductVM. Implemented updating of categories (tags) for products.

--- Bookshop.DataAccess/Repository/IRepository/IProductRepository.cs
+++ Bookshop.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -4,11 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bookshop.Models.ViewModels;
 
 namespace Bookshop.DataAccess.Repository.IRepository
 {
     public interface IProductRepository : IRepository<Product>
     {
         void Update(Product product);
+        public Task Update(ProductVM productVm);
+
+        public Task Add(ProductVM productVm);
+
     }
 }
--- Bookshop.DataAccess/Repository/ProductRepository.cs
+++ Bookshop.DataAccess/Repository/ProductRepository.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 using Bookshop.DataAccess.Data;
 using Bookshop.DataAccess.Repository.IRepository;
 using Bookshop.Models;
+using Bookshop.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bookshop.DataAccess.Repository
 {
@@ -17,9 +20,39 @@ namespace Bookshop.DataAccess.Repository
         {
             this.DbContext = dbContext;
         }
+        public async Task Update(ProductVM productVm)
+        {
+            var product = productVm.Product;
+            var productFromDb = await DbContext.Products.Include(x => x.Categories).FirstOrDefaultAsync(x => x.Id == product.Id);
+            if (productFromDb is null)
+            {
+                DbContext.Products.Update(product);
+                return;
+            }
+
+            productFromDb.Categories.Clear();
+            await DbContext.SaveChangesAsync();
+            DbContext.Entry(productFromDb).CurrentValues.SetValues(product);
+            var availableCategories = await DbContext.Categories.ToListAsync();
+            foreach (var categoryId in productVm.CategoryIds)
+            {
+                productFromDb.Categories.Add(availableCategories.First(x => x.Id == categoryId));
+            }
+        }
+
         public void Update(Product product)
         {
             DbContext.Products.Update(product);
+        }
+
+        public async Task Add(ProductVM productVm)
+        {
+            var availableCategories = await DbContext.Categories.ToListAsync();
+            foreach (var categoryId in productVm.CategoryIds)
+            {
+                productVm.Product.Categories.Add(availableCategories.First(x => x.Id == categoryId));
+            }
+            DbContext.Products.Add(productVm.Product);
         }
     }
 }
