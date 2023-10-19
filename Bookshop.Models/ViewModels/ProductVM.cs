ec291c42bea5102844804cb63b04c436d0e1497f
Geronimo <lopez.geronimo213@gmail.com>
2023-10-19 10:13:17 -0500

    Added list for passing back selected categories.

--- Bookshop.Models/ViewModels/ProductVM.cs
+++ Bookshop.Models/ViewModels/ProductVM.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -13,5 +15,7 @@ namespace Bookshop.Models.ViewModels
         public Product Product { get; set; } = new Product();
         [ValidateNever]
         public IEnumerable<SelectListItem> CategoryList { get; set; } = Enumerable.Empty<SelectListItem>();
+
+        [ValidateNever] public List<int> CategoryIds { get; set; } = new List<int>();
     }
 }
