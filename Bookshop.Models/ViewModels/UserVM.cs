0257b81cc1ee470e581f4c291768cb417d360b25
Geronimo <lopez.geronimo213@gmail.com>
2023-10-23 17:14:50 -0500

    created userVM. May not end up using. will see

---
+++ Bookshop.Models/ViewModels/UserVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Bookshop.Models.ViewModels
+{
+    public class UserVM
+    {
+        public AppUser User { get; set; }
+        public IdentityRole Role { get; set; }
+    }
+}
