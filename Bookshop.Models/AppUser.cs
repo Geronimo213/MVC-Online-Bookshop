6cd5180b9057d0da1605ef92228f00d299833f98
Geronimo <lopez.geronimo213@gmail.com>
2023-10-23 17:15:17 -0500

    Added non-mapped Role property for passing role name along via join.

--- Bookshop.Models/AppUser.cs
+++ Bookshop.Models/AppUser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,5 +18,8 @@ namespace Bookshop.Models
         public string? City { get; set; }
         public string? State { get; set; }
         public string? PostalCode { get; set;}
+
+        [NotMapped]
+        public string? Role { get; set; }
     }
 }
