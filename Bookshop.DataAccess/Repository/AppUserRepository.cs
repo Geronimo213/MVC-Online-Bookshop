642591b4b9be5f1416ca7f7c4d5adbca5e1d2a60
Geronimo <lopez.geronimo213@gmail.com>
2023-10-24 11:16:06 -0500

    Created method to get all users and associated roles via custom query (moved from UserController)

--- Bookshop.DataAccess/Repository/AppUserRepository.cs
+++ Bookshop.DataAccess/Repository/AppUserRepository.cs
@@ -18,6 +18,31 @@ namespace Bookshop.DataAccess.Repository
             this._dbContext = dbContext;
         }
 
+        public IQueryable<AppUser> GetAllWithRoles()
+        {
+             var query = (from user in DbSet
+                join ur in _dbContext.UserRoles on user.Id equals ur.UserId
+                join r in _dbContext.Roles on ur.RoleId equals r.Id
+                select new AppUser()
+                {
+                    Id = user.Id,
+                    Name = user.Name,
+                    Email = user.Email,
+                    NormalizedEmail = user.NormalizedEmail,
+                    City = user.City,
+                    State = user.State,
+                    PostalCode = user.PostalCode,
+                    LockoutEnabled = user.LockoutEnabled,
+                    LockoutEnd = user.LockoutEnd,
+                    PhoneNumber = user.PhoneNumber,
+                    StreetAddress = user.StreetAddress,
+                    UserName = user.UserName,
+                    NormalizedUserName = user.NormalizedUserName,
+                    Role = r.Name
+                });
+            return query;
+        }
+
         //public void Update(AppUser appUser)
         //{
         //    _dbContext.AppUsers.Update(appUser);
--- Bookshop.DataAccess/Repository/IRepository/IAppUserRepository.cs
+++ Bookshop.DataAccess/Repository/IRepository/IAppUserRepository.cs
@@ -10,5 +10,6 @@ namespace Bookshop.DataAccess.Repository.IRepository
     public interface IAppUserRepository : IRepository<AppUser>
     {
         //void Update(AppUser appUser);
+        public IQueryable<AppUser> GetAllWithRoles();
     }
 }
