86854ede2b5aa27a8f09dc075c8df66b79e36b6f
Geronimo <lopez.geronimo213@gmail.com>
2023-10-24 11:16:24 -0500

    altered userVM to be used for changing of role

--- Bookshop.Models/ViewModels/UserVM.cs
+++ Bookshop.Models/ViewModels/UserVM.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Bookshop.Models.ViewModels
 {
     public class UserVM
     {
-        public AppUser User { get; set; }
-        public IdentityRole Role { get; set; }
+        public AppUser User { get; set; } = new AppUser();
+        public IEnumerable<SelectListItem> RoleList { get; set; } = Enumerable.Empty<SelectListItem>();
+
     }
 }
