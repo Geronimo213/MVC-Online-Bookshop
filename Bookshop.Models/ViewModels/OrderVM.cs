7ab272043b41593af77a1153ad475c923fa9874e
Geronimo <lopez.geronimo213@gmail.com>
2023-10-26 12:04:46 -0500

    Remove Utility -> Models dependancy by moving PaginatedList<OrderVM> definition to OrderVM class file.

--- Bookshop.Models/ViewModels/OrderVM.cs
+++ Bookshop.Models/ViewModels/OrderVM.cs
@@ -1,14 +1,45 @@
-﻿namespace Bookshop.Models.ViewModels
+﻿using Bookshop.Utility;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookshop.Models.ViewModels
 {
     public class OrderVM
     {
-        public Order Header { get; set; }
-        public IEnumerable<OrderLines> Lines { get; set; }
+        public Order Header { get; set; } = new Order();
+        public IEnumerable<OrderLines> Lines { get; set; } = Enumerable.Empty<OrderLines>();
 
-        public OrderVM()
+
+    }
+
+    public class PaginatedOrders : PaginatedList<OrderVM>
+    {
+        public PaginatedOrders(int count, int pageIndex, int pageSize) : base(count, pageIndex, pageSize)
         {
-            this.Header = new Order();
-            this.Lines = new List<OrderLines>();
+            
+        }
+        public static async Task<PaginatedList<OrderVM>> CreateAsync(IQueryable<Order> source, IQueryable<OrderLines> joinSource, int pageIndex, int pageSize, string? includeOperators)
+        {
+            var count = await source.CountAsync();
+            pageIndex = Math.Min(pageIndex, (int)Math.Ceiling(count / (double)pageSize));
+            pageIndex = pageIndex < 1 ? 1 : pageIndex;
+            var orders = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            var orderList = new List<OrderVM>();
+            foreach (var order in orders)
+            {
+                var orderVM = new OrderVM
+                {
+                    Header = order
+                };
+                var lineQuery = joinSource.Where(x => x.OrderId == order.OrderId);
+                if (!string.IsNullOrEmpty(includeOperators))
+                {
+                    lineQuery = includeOperators.Split(',', StringSplitOptions.RemoveEmptyEntries).Aggregate(lineQuery, (current, includeOperator) => current.Include(includeOperator));
+                }
+                orderVM.Lines = lineQuery;
+                orderList.Add(orderVM);
+            }
+
+            return new PaginatedList<OrderVM>(orderList, count, pageIndex, pageSize);
         }
     }
 }
--- Bookshop.Utility/PaginatedList.cs
+++ Bookshop.Utility/PaginatedList.cs
@@ -1,5 +1,4 @@
-﻿using Bookshop.Models;
-using Bookshop.Models.ViewModels;
+﻿
 using Microsoft.EntityFrameworkCore;
 
 
@@ -12,16 +11,16 @@ namespace Bookshop.Utility
 
         public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
         {
-            PageIndex = pageIndex;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            PageIndex = pageIndex;
 
             this.AddRange(items);
         }
 
         public PaginatedList(int count, int pageIndex, int pageSize)
         {
-            PageIndex = pageIndex;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            PageIndex = pageIndex;
         }
 
         public bool HasPreviousPage => PageIndex > 1;
@@ -36,33 +35,7 @@ namespace Bookshop.Utility
             var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
             return new PaginatedList<T>(items, count, pageIndex, pageSize);
         }
-        public static async Task<PaginatedList<OrderVM>> CreateAsync(IQueryable<Order> source, IQueryable<OrderLines> joinSource, int pageIndex, int pageSize, string? includeOperators)
-        {
-            var count = await source.CountAsync();
-            pageIndex = Math.Min(pageIndex, (int)Math.Ceiling(count / (double)pageSize));
-            pageIndex = pageIndex < 1 ? 1 : pageIndex;
-            var orders = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
-            var orderList = new List<OrderVM>();
-            foreach (var order in orders)
-            {
-                var orderVM = new OrderVM
-                {
-                    Header = order
-                };
-                var lineQuery = joinSource.Where(x => x.OrderId == order.OrderId);
-                if (!String.IsNullOrEmpty(includeOperators))
-                {
-                    foreach (var includeOperator in includeOperators.Split(',', StringSplitOptions.RemoveEmptyEntries))
-                    {
-                        lineQuery = lineQuery.Include(includeOperator);
-                    }
-                }
-                orderVM.Lines = lineQuery;
-                orderList.Add(orderVM);
-            }
 
-            return new PaginatedList<OrderVM>(orderList, count, pageIndex, pageSize);
-        }
     }
 
 
