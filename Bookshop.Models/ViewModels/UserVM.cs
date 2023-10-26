4c5a6506d97ddfe38eab1a6fbae7a9cb4b866964
Geronimo <lopez.geronimo213@gmail.com>
2023-10-26 12:07:32 -0500

    Added return list for selected roles.

--- Bookshop.Models/ViewModels/UserVM.cs
+++ Bookshop.Models/ViewModels/UserVM.cs
@@ -7,5 +7,7 @@ namespace Bookshop.Models.ViewModels
         public AppUser User { get; set; } = new AppUser();
         public IEnumerable<SelectListItem> RoleList { get; set; } = Enumerable.Empty<SelectListItem>();
 
+        public List<string> SelectedRoles { get; set; } = new List<string>();
+
     }
 }
