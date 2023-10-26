ae0aca233db2f7f1f1b56913a9dc1880ef6949d1
Geronimo <lopez.geronimo213@gmail.com>
2023-10-26 12:08:04 -0500

    Added ViewModel for specific use in pagination (Index action)

---
+++ Bookshop.Models/ViewModels/PaginatedUserVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bookshop.Utility;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bookshop.Models.ViewModels
+{
+    public class PaginatedUserVM
+    {
+
+        public PaginatedList<AppUser> Users { get; set; }
+        public IEnumerable<SelectListItem> RoleList { get; set; } = Enumerable.Empty<SelectListItem>();
+
+        public List<string> SelectedRoles { get; set; } = new List<string>();
+    }
+}
