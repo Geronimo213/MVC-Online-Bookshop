e8c50c7e31f37517a85c451f4db9f7045ce6f1c1
Geronimo <lopez.geronimo213@gmail.com>
2023-10-30 15:55:29 -0500

    Added view model for paginated products. For customer facing use.

---
+++ Bookshop.Models/ViewModels/PaginatedProductVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bookshop.Utility;
+
+namespace Bookshop.Models.ViewModels
+{
+    public class PaginatedProductVM
+    {
+        public readonly List<string> SortParams = new (){ "Title", "Author" };
+        public PaginatedList<Product> Products { get; set; } = new PaginatedList<Product>(0, 1, 1);
+        public string? SortOrder { get; set; }
+        public string? SearchParam { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
