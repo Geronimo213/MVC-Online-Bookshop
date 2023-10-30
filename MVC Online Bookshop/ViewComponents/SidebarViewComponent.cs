a2a2937d7b087fef32878e91a7739d8d6bb229dc
Geronimo <lopez.geronimo213@gmail.com>
2023-10-30 15:59:46 -0500

    Added sidebar for shop (browse) view. Gets list of categories and count of products within. Books counted twice if overlap.

---
+++ MVC Online Bookshop/ViewComponents/SidebarViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Bookshop.DataAccess.Repository.IRepository;
+using Bookshop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Online_Bookshop.ViewComponents
+{
+    public class SidebarViewComponent : ViewComponent
+    {
+        private IUnitOfWork UnitOfWork { get; }
+
+        public SidebarViewComponent(IUnitOfWork unitOfWork)
+        {
+            this.UnitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string? currentCategory, bool? displayBadges)
+        {
+            ViewData["DisplayBadges"] = displayBadges ?? true;
+            ViewData["CategoryParam"] = currentCategory ?? "";
+            var categories = await UnitOfWork.CategoryRepository
+                .GetAll()
+                .OrderBy(x => x.DisplayOrder)
+                .Select(x => new Category() 
+                    { DisplayOrder = 
+                        x.DisplayOrder,
+                        Id = x.Id,
+                        Name = x.Name,
+                        ProductCount = x.Products.Count
+                    })
+                .ToListAsync();
+
+            return View(categories);
+        }
+    }
+}
