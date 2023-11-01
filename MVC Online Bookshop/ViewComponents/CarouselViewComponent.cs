6555f3f641cde003a7871e7be6519401fe967849
Geronimo <lopez.geronimo213@gmail.com>
2023-11-01 16:15:22 -0500

    Carousel ViewComponent for usage anywhere

---
+++ MVC Online Bookshop/ViewComponents/CarouselViewComponent.cs
@@ -0,0 +1,55 @@
+﻿using Bookshop.DataAccess.Repository.IRepository;
+using Bookshop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Online_Bookshop.ViewComponents
+{
+    public class CarouselViewComponent : ViewComponent
+    {
+        private IUnitOfWork UnitOfWork { get; }
+
+        public CarouselViewComponent(IUnitOfWork unitOfWork)
+        {
+            this.UnitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(Carousel carousel)
+        {
+            var categoryDb = await UnitOfWork.CategoryRepository.Get(x => x.Id == carousel.CategoryId);
+            if (categoryDb is null) return View();
+
+            var productsDb = UnitOfWork.ProductRepository.GetAll(x => x.Categories.Contains(categoryDb));
+            if (productsDb is null) return View();
+
+            var sortOrder = carousel.SortOrder;
+
+            productsDb= sortOrder switch
+            {
+                "Author" => productsDb.OrderBy(x => x.Author),
+                "Title" => productsDb.OrderBy(x => x.Title),
+                _ => productsDb.OrderBy(x => x.Id)
+            };
+
+
+            var vm = new CarouselVM()
+            {
+                Products = await productsDb.ToListAsync(),
+                Title = carousel.Title
+            };
+            return View(vm);
+        }
+    }
+
+    public class CarouselVM
+    {
+        public string Title { get; set; }
+        public List<Product> Products { get; set; }
+
+        public CarouselVM()
+        {
+            this.Products = new List<Product>();
+            this.Title = string.Empty;
+        }
+    }
+}
