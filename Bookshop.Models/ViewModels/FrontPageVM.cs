79a7b936b919c8d4080d4160645cfb553be999ea
Geronimo <lopez.geronimo213@gmail.com>
2023-11-01 16:15:34 -0500

    ViewModel for front page of website

---
+++ Bookshop.Models/ViewModels/FrontPageVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookshop.Models.ViewModels
+{
+    public class FrontPageVM
+    {
+
+        public List<Carousel> Carousels { get; set; } = new List<Carousel>();
+
+        public string Title { get; set; } = "Welcome to Ye Olde Bookshop!";
+
+    }
+}
