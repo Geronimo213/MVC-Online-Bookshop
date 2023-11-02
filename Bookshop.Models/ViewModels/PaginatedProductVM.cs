b0739b643f690db3ed987e8e1c209ce3b1f2fa66
Geronimo <lopez.geronimo213@gmail.com>
2023-11-02 09:16:16 -0500

    Changed name of SearchParam to CurrentFilter to more accurately describe it's purpose.

--- Bookshop.Models/ViewModels/PaginatedProductVM.cs
+++ Bookshop.Models/ViewModels/PaginatedProductVM.cs
@@ -13,7 +13,7 @@ namespace Bookshop.Models.ViewModels
         public readonly List<string> SortParams = new (){ "Title", "Author" };
         public PaginatedList<Product> Products { get; set; } = new PaginatedList<Product>(0, 1, 1);
         public string? SortOrder { get; set; }
-        public string? SearchParam { get; set; }
+        public string? CurrentFilter { get; set; }
         public string CategoryName { get; set; } = string.Empty;
     }
 }
