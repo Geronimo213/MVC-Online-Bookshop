ff21d36f4a1eb2af0487420798eebf01794337ab
Geronimo <lopez.geronimo213@gmail.com>
2023-11-02 12:19:49 -0500

    VM for front page manager.

---
+++ Bookshop.Models/ViewModels/FrontPageManageVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookshop.Models.ViewModels
+{
+    public class FrontPageManageVM
+    {
+        public List<Carousel> Carousels { get; set; } = new();
+        public List<Header> Headers { get; set; } = new();
+    }
+}
