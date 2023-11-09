b4c841d6b6436b3b8cb61799ddaba7e21e362a8d
Geronimo <lopez.geronimo213@gmail.com>
2023-11-09 17:00:36 -0600

    Added status and stripe id update methods for Order header

--- Bookshop.DataAccess/Repository/IRepository/IOrderRepository.cs
+++ Bookshop.DataAccess/Repository/IRepository/IOrderRepository.cs
@@ -5,5 +5,9 @@ namespace Bookshop.DataAccess.Repository.IRepository
     public interface IOrderRepository : IRepository<Order>
     {
         void Update(Order order);
+
+        void UpdateStatus(int id, string orderStatus);
+
+        void UpdateStripe(int id, string sessionId, string paymentId);
     }
 }
--- Bookshop.DataAccess/Repository/OrderRepository.cs
+++ Bookshop.DataAccess/Repository/OrderRepository.cs
@@ -16,5 +16,32 @@ namespace Bookshop.DataAccess.Repository
         {
             DbContext.Orders.Update(order);
         }
+
+        public void UpdateStatus(int id, string orderStatus)
+        {
+            var orderDb = DbContext.Orders.FirstOrDefault(x => x.OrderId == id);
+
+            if (orderDb == null) return;
+            if (!string.IsNullOrEmpty(orderStatus))
+            {
+                orderDb.OrderStatus = orderStatus;
+            }
+        }
+
+        public void UpdateStripe(int id, string sessionId, string paymentId)
+        {
+            var orderDb = DbContext.Orders.FirstOrDefault(x => x.OrderId == id);
+
+            if (orderDb == null) return;
+            if (!string.IsNullOrEmpty(sessionId))
+            {
+                orderDb.SessionId = sessionId;
+            }
+
+            if (!string.IsNullOrEmpty(paymentId))
+            {
+                orderDb.PaymentIntentId = paymentId;
+            }
+        }
     }
 }
