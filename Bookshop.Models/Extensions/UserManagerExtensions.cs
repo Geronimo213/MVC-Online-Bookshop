cf108c5098dcf37a1c37147778acfd09ac6f7cd3
Geronimo <lopez.geronimo213@gmail.com>
2023-11-13 14:20:46 -0600

    ended up not using this but the outline might be useful later

---
+++ Bookshop.Models/Extensions/UserManagerExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bookshop.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Bookshop.Utility
+{
+    public static class UserManagerExtensions
+    {
+        public static Task<string?> GetUserAddressAsync(this UserManager<AppUser> userManager, AppUser user)
+        {
+            return Task.FromResult(user.StreetAddress);
+        }
+    }
+}
