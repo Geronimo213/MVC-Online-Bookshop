1a756185c0f4485ef3f76641330acffbc58ac6dc
Geronimo <lopez.geronimo213@gmail.com>
2023-11-14 18:20:52 -0600

    email sender actually sends . . .emails

--- Bookshop.Utility/EmailSender.cs
+++ Bookshop.Utility/EmailSender.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace Bookshop.Utility
 {
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        public string SendGridSecret { get; set; }
+
+        public EmailSender(IConfiguration cfg)
         {
-            return Task.CompletedTask;
+            this.SendGridSecret = cfg.GetValue<string>("SendGrid:SecretKey")!;
+        }
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var client = new SendGridClient(SendGridSecret);
+
+            var from = new EmailAddress("noreply@geronimo.design", "Ye Olde Bookshop");
+            var to = new EmailAddress(email);
+            var message = MailHelper.CreateSingleEmail(from, to, subject,"", htmlMessage);
+
+            await client.SendEmailAsync(message);
         }
     }
 }
