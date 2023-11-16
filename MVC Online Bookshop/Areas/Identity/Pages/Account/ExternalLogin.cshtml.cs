605bd352e4491ca902e9c77974938ebe9dcbd263
Geronimo <lopez.geronimo213@gmail.com>
2023-11-16 14:27:07 -0600

    Implemented two facto ran drequired two-factor on external login. Also moved social login buttons into partials and implemented displaying of logos on external accounts management page.

--- MVC Online Bookshop/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ MVC Online Bookshop/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -110,15 +110,20 @@ namespace MVC_Online_Bookshop.Areas.Identity.Pages.Account
             }
 
             // Sign in the user with this external login provider if the user already has a login.
-            var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
+            var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: false);
             if (result.Succeeded)
             {
-                _logger.LogInformation("{Name} logged in with {LoginProvider} provider.", info.Principal.Identity.Name, info.LoginProvider);
+                _logger.LogInformation("{Name} logged in with {LoginProvider} provider.", info.Principal.Identity?.Name, info.LoginProvider);
                 return LocalRedirect(returnUrl);
             }
             if (result.IsLockedOut)
             {
                 return RedirectToPage("./Lockout");
+            }
+
+            if (result.RequiresTwoFactor)
+            {
+                return RedirectToPage("./LoginWith2fa", new { returnUrl = returnUrl, rememberMe = false});
             }
             else
             {
