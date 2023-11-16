40cb51f91352c5e46d7958c86fe021054bcde21a
Geronimo <lopez.geronimo213@gmail.com>
2023-11-16 10:32:12 -0600

    extension method to get base url from request

---
+++ Bookshop.Utility/HttpRequestExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Bookshop.Utility
+{
+    public static class HttpRequestExtensions
+    {
+        public static Task<string?> GetBaseUrl(this HttpRequest? request)
+        {
+            if (request == null) return Task.FromResult<string?>(null);
+
+            var uriBuilder = new UriBuilder(request.Scheme, request.Host.Host, request.Host.Port ?? -1);
+            if (uriBuilder.Uri.IsDefaultPort)
+            {
+                uriBuilder.Port = -1;
+            }
+
+            return Task.FromResult<string?>(uriBuilder.Uri.AbsoluteUri);
+
+        }
+    }
+}
