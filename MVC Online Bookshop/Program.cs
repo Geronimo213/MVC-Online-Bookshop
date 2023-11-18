eb1ad6fa20535f7d3239e1b5d600985c3f24a0ce
Geronimo <lopez.geronimo213@gmail.com>
2023-11-17 21:32:10 -0600

    connection strings env variables

--- MVC Online Bookshop/Program.cs
+++ MVC Online Bookshop/Program.cs
@@ -11,6 +11,7 @@ using Stripe;
 using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddEnvironmentVariables();
 
 var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
 builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
