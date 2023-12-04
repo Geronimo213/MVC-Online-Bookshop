efc7690a41ed55ef9a8a0f5ca330748227efa5f4
Geronimo <lopez.geronimo213@gmail.com>
2023-12-04 15:29:05 -0600

    lists show up in second nav row. Updated list ui. Added share link with copy link button. Fixed front page centering. Carousel indicators removed on small screens. Moved navbar into its own partial. View component for displaying lists in nav. List controller created.

---
+++ MVC Online Bookshop/ViewComponents/NavbarListsViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Bookshop.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Online_Bookshop.ViewComponents
+{
+    public class NavbarListsViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public NavbarListsViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var lists = await _unitOfWork.BookListRepository.GetAll().AsNoTracking().ToListAsync();
+            return View(lists);
+        }
+    }
+}
