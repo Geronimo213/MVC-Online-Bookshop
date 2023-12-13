14ff989fe30abbdb85bf8312d0e35ebed23daa7b
Geronimo <lopez.geronimo213@gmail.com>
2023-12-13 16:22:58 -0600

    Added column SessionId, which is an index and required. Made user FK optional.

--- Bookshop.Models/ShoppingCart.cs
+++ Bookshop.Models/ShoppingCart.cs
@@ -1,27 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bookshop.Models
 {
+    [Index(nameof(SessionId))]
     public class ShoppingCart
     {
         [Key]
         public int Id { get; set; }
-        public double Total { get; set; }
 
+      [ForeignKey("Product")]
         public int ProductId { get; set; }
-        [ForeignKey("ProductId")]
+  
         [ValidateNever]
         public Product Product { get; set; }
 
         [Range(1, 5, ErrorMessage = "Limit 5 per person.")]
         public int Count { get; set; }
-
-        public string UserId { get; set; }
-        [ForeignKey("UserId")]
+        [ForeignKey("User")]
+        public string? UserId { get; set; }
         [ValidateNever]
         public AppUser? User { get; set; }
-
+        [Required]
+        public string SessionId { get; set; }
     }
 }
