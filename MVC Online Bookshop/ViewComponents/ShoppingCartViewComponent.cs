3998c72a70cb24d97ec796d21cdb04464bf8f142
Geronimo <lopez.geronimo213@gmail.com>
2023-12-13 16:29:06 -0600

    Cart now checks DB every time to see if changes have been made. Might be faster solution in future. Will look into it.

--- MVC Online Bookshop/ViewComponents/ShoppingCartViewComponent.cs
+++ MVC Online Bookshop/ViewComponents/ShoppingCartViewComponent.cs
@@ -9,7 +9,6 @@ namespace MVC_Online_Bookshop.ViewComponents
     public class ShoppingCartViewComponent : ViewComponent
     {
         private IUnitOfWork UnitOfWork { get; }
-
         public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
         {
             this.UnitOfWork = unitOfWork;
@@ -19,18 +18,29 @@ namespace MVC_Online_Bookshop.ViewComponents
         {
             var claimsIdentity = (ClaimsIdentity?)User.Identity;
             var claim = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier);
+            var cartCookie = Request.Cookies[SD.ShoppingCartCookie];
+
 
             if (claim is not null)
             {
-                if (HttpContext.Session.GetInt32(SD.SessionCart) is null)
-                {
-                    HttpContext.Session.SetInt32(SD.SessionCart,
-                        await UnitOfWork.ShoppingCartRepository.GetAll().CountAsync(x => x.UserId == claim.Value));
-                }
+
+                HttpContext.Session.SetInt32(SD.SessionCart,
+                    await UnitOfWork.ShoppingCartRepository.GetAll().CountAsync(x => x.UserId == claim.Value));
+
 
                 return View((int)HttpContext.Session.GetInt32(SD.SessionCart)!);
             }
 
+            if (cartCookie is not null)
+            {
+                if (HttpContext.Session.GetInt32(SD.SessionCart) is null)
+                {
+                    HttpContext.Session.SetInt32(SD.SessionCart,
+                        await UnitOfWork.ShoppingCartRepository.GetAll().CountAsync(x => x.SessionId == cartCookie));
+                }
+
+                return View((int)HttpContext.Session.GetInt32(SD.SessionCart)!);
+            }
 
             HttpContext.Session.Clear();
             return View(0);
