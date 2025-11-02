801a24221e3374f2d54224f147b64c05efb0f480
Geronimo <lopez.geronimo213@gmail.com>
2025-11-02 09:55:06 -0600

    trying to fix

--- MVC Online Bookshop/Program.cs
+++ MVC Online Bookshop/Program.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Stripe;
-using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddEnvironmentVariables();
